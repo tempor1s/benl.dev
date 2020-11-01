cde57541c86220eecefc247e243413d8a70f3cb8
Ben <benlaugherty@gmail.com>
2020-11-01 09:48:23 -0800

    black background

--- src/shared-styles/global.styles.js
+++ src/shared-styles/global.styles.js
@@ -5,18 +5,10 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: 'Poppins', sans-serif, Arial, Helvetica !important;
+        background-color: #151515
     }
 
     #root {
         overflow: hidden;
-    }
-
-    html:before {
-        content: '';
-        position: fixed;
-        width: 100%;
-        height: 999px;
-        top: -999px;
-        background-color: #151515
     }
 `;
