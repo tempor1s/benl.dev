f5fda0a038c0f9a478e5a6399c87364e24dcd8aa
Ben <benlaugherty@gmail.com>
2020-11-06 16:21:25 -0500

    fix background for now

--- src/shared-styles/global.styles.js
+++ src/shared-styles/global.styles.js
@@ -5,7 +5,6 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: 'Poppins', sans-serif, Arial, Helvetica !important;
-        background-color: #151515
     }
 
     #root {
