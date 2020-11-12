d0d3340bf0ff942b26659020d19aad287ff91d8c
Ben <benlaugherty@gmail.com>
2020-11-12 13:10:43 -0700

    add social links for basic website mvp

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
-
 import { device } from '../../utils/theme.util';
+
+import { Github } from '@styled-icons/boxicons-logos/Github';
+import { LinkedinSquare } from '@styled-icons/boxicons-logos/LinkedinSquare';
+import { Email } from '@styled-icons/material/Email';
 
 const Hero = styled.div`
     display: flex;
@@ -35,11 +38,26 @@ const SubHeader = styled.h3`
     }
 `;
 
+const Icons = styled.div`
+    padding-top: 8px; 
+    
+    a {
+        text-decoration: none;
+        color: inherit;
+        margin-right: 8px;
+    }
+`;
+
 export function Home() {
     return (
         <Hero>
             <Header>Ben Lafferty</Header>
             <SubHeader>Backend & Systems Developer</SubHeader>
+            <Icons>
+                <a href={"https://github.com/tempor1s"} target={"_blank"} rel={"noreferrer"}><Github size={"35"} title={"GitHub"} /></a>
+                <a href={"https://linkedin.com/in/benlaugherty"} target={"_blank"} rel={"noreferrer"}><LinkedinSquare size={"35"} title={"LinkedIn"} /></a>
+                <a href={"mailto:me@benl.dev"} target={"_blank"} rel={"noreferrer"}><Email size={"35"} title={"Email"} /></a>
+            </Icons>
         </Hero>
     );
 }
